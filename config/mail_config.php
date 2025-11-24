343604e72460453dd10ebbe25ab33adf34deced9
DESKTOP-HHGOI4D\Justo <justo147@hotmail.com>
2025-11-24 12:59:50 +0100

    cambio de conf de mail_conf

--- config/mail_config.php
+++ config/mail_config.php
@@ -4,31 +4,33 @@ require __DIR__ . '/../vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-function sendMail($toEmail, $toName, $subject, $htmlBody, $altBody = ''): bool {
+function sendMail($toEmail, $toName, $subject, $htmlBody, $altBody = ''): bool
+{
   $mail = new PHPMailer(true);
-try {
-  $mail->isSMTP();
-  $mail->Host = 'smtp.office365.com';
-  $mail->SMTPAuth = true;
-  $mail->Username = 'sergiodaw2025@outlook.com';
-  $mail->Password = 'tuzvwpiimnwtgbrz';
-  $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-  $mail->Port = 587;
+  $mail->SMTPDebug = 2; // o 3 para más detalle
+  $mail->Debugoutput = 'html';
 
-  $mail->setFrom('sergiodaw2025@outlook.com', 'MercaAPP');
-  $mail->addAddress($toEmail, $toName);
+  try {
+    $mail->isSMTP();
+    $mail->Host = 'smtp.office365.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'sergiodaw2025@outlook.es';
+    $mail->Password = 'tuzvwpiimnwtgbrz';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
 
-  $mail->isHTML(true);
-  $mail->Subject = $subject;
-  $mail->Body = $htmlBody;
-  $mail->AltBody = $altBody ?: strip_tags($htmlBody);
+    $mail->setFrom('sergiodaw2025@outlook.es', 'MercaAPP');
+    $mail->addAddress($toEmail, $toName);
 
-  $mail->send();
-  return true;
-} catch (Exception $e) {
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->Body = $htmlBody;
+    $mail->AltBody = $altBody ?: strip_tags($htmlBody);
+
+    $mail->send();
+    return true;
+  } catch (Exception $e) {
     echo "Error al enviar correo: {$mail->ErrorInfo}";
     return false;
-}
-
-
+  }
 }
