360bb83a522567ab81a02d00fdd1a57927fae144
Justo Puerto <justo147@hotmail.com>
2026-01-22 08:52:53 +0100

    agregado popper en home

--- public/views/home.php
+++ public/views/home.php
@@ -18,7 +18,7 @@ require_once __DIR__ . '/../../controllers/handlers/home_handlers.php';
   <!-- CSS personalizados -->
   <link rel="stylesheet" href="../css/reset.css">
   <link rel="stylesheet" href="../css/homeStyle.css">
-  <link rel="stylesheet" href="../css/style-guide.css">
+  <!-- <link rel="stylesheet" href="../css/style-guide.css"> -->
 
   <!-- JS para tema oscuro/claro -->
   <script src="../js/theme.js" defer></script>
@@ -26,7 +26,8 @@ require_once __DIR__ . '/../../controllers/handlers/home_handlers.php';
   <!-- Bootstrap CSS y JS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
-  
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
 
   <!-- Bootstrap Icons -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
@@ -71,7 +72,7 @@ require_once __DIR__ . '/../../controllers/handlers/home_handlers.php';
 </body>
 <footer>
 
-        <?php include __DIR__ . '/footer.php'; ?>
+  <?php include __DIR__ . '/footer.php'; ?>
 
 </footer>
 
