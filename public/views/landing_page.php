34a35184d5b89724fe905a4ef9b8afea68f7fce4
Juan Jesus <juanjesusgarcia279@gmail.com>
2026-01-27 09:33:27 +0100

    pagina scss añadido a la pagina help y cambios de posicion en help

--- public/views/landing_page.php
+++ public/views/landing_page.php
@@ -5,16 +5,16 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>MercApp - Compra y Venta</title>
-  
+
   <!-- Favicon de la aplicación -->
   <link rel="icon" href="../ico/logo_sinfondo.ico" type="image/x-icon">
   <link rel="shortcut icon" href="../ico/logo_sinfondo.ico" type="image/x-icon">
-  
+
   <!-- CSS -->
-  <link rel="stylesheet" href="../css/reset.css">        <!-- Reset minimalista -->
-  <link rel="stylesheet" href="../css/style-guide.css">  <!-- Variables y estilos globales -->
-  <link rel="stylesheet" href="../css/style.css">        <!-- Estilos específicos de la landing -->
-  
+  <link rel="stylesheet" href="../css/reset.css"> <!-- Reset minimalista -->
+  <link rel="stylesheet" href="../css/style-guide.css"> <!-- Variables y estilos globales -->
+  <link rel="stylesheet" href="../css/style.css"> <!-- Estilos específicos de la landing -->
+
   <!-- JS controlador de landing (manejo de botones de login/registro) -->
   <script src="../../controllers/landing_controllers.js"></script>
 </head>
@@ -36,5 +36,7 @@
     NOTA: La navegación de los botones se gestiona mediante 
     landing_controllers.js, que redirige a login.php o register.php.
   -->
+
 </body>
+
 </html>
